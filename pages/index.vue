719d025af7c739d542196f7e03a3f3f11da7825e
AnnaNee <annaflaviac@me.com>
2021-01-23 21:32:20 -0300

    Add centered content section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <h1>Bravado Challenge</h1>
+  <div class="content">
+    <h1>Bravado Challenge</h1>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+.content {
+  width: 564px;
+  height: 643px;
+  background-color: $white;
+  padding: 0.75rem;
+  @include vertical-align;
+}
+</style>
