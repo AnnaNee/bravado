71b40cf51dfafc369e13ad1663f22f0af09564e9
AnnaNee <annaflaviac@me.com>
2021-01-26 11:04:29 -0300

    Create SearchBar component

---
+++ components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="search-bar">
+    <i class="search-bar__icon"></i>
+
+    <input
+      v-model="search"
+      class="search-bar__input"
+      type="text"
+      placeholder="Type your search..."
+      @keyup="handleSearch()"
+    />
+  </div>
+</template>
+
+<script>
+const debounce = (fn, wait) => {
+  let timeout
+  return function () {
+    const context = this
+    clearTimeout(timeout)
+    timeout = setTimeout(fn.bind(context), wait)
+  }
+}
+
+export default {
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    handleSearch: debounce(function () {
+      this.$emit('on-search', this.search)
+    }, 1000),
+  },
+}
+</script>
